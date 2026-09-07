2cd02d4b12b61372cedb1d2fc5a9f79c1c08c7d8
Arsen Hevorkian <ahevorkian@cdtnue.onmicrosoft.com>
2026-09-07 16:05:08 +0200

    Übungen 7.09.26

---
+++ src/Seos/UebMethoden.java
@@ -0,0 +1,80 @@
+package Seos;
+
+import java.util.Scanner;
+
+public class UebMethoden {
+
+	public static void main(String[] args) {
+		
+		int[] zahlen = werteEingabe();
+		
+		int summe = summeVonArray(zahlen);
+		
+		ausgabe(summe);
+		
+		int[] zahlenVerdoppelt = arrayVerdoppeln(zahlen);
+		
+		ausgabe(zahlen);
+		ausgabe(zahlenVerdoppelt);
+
+	}
+
+	public static int[] werteEingabe() {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Geben si Anzahl der Werte ein!");
+
+		int[] arr = new int[sc.nextInt()];
+
+		for (int i = 0; i < arr.length; i++) {
+
+			System.out.println("Geben Sie " + i + " element von Array");
+			arr[i] = sc.nextInt();
+		}
+
+		sc.close();
+
+		return arr;
+
+	}
+
+	public static int summeVonArray(int[] arr) {
+
+		int summe = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+
+			summe = summe + arr[i];
+		}
+		return summe;
+	}
+
+	public static void ausgabe(int summe) {
+
+		System.out.println("Die Summe des Arrays = " + summe);
+	}
+
+	public static void ausgabe(int[] arr) {
+
+		//System.out.println("Array = ");
+
+		for (int i = 0; i < arr.length; i++) {
+
+			System.out.print("[" + arr[i] + "]");
+		}
+		System.out.println("");
+	}
+	
+	public static int[] arrayVerdoppeln(int[] arr) {
+		
+		// !!! Wichtig neuen Array zu erstellen!!!
+		int[] array2 = new int[arr.length];
+
+		for (int i = 0; i < arr.length; i++) {
+
+			array2[i] = arr[i] * 2;
+		}
+		return array2;
+	}
+
+}
---
+++ src/cdt/UebBooleanBerachnen.java
@@ -0,0 +1,15 @@
+package cdt;
+
+public class UebBooleanBerachnen {
+
+	public static void main(String[] args) {
+		
+		double a = 0.1;
+		double b = 0.2;
+		double result = a + b;
+		
+		System.out.println(result);
+
+	}
+
+}
